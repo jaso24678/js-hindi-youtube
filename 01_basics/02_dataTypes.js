12177b2be09389521215d7323cd19f1aecbc03af
jaso24678 <jasobantapadhan.16001@gmail.com>
2025-10-28 14:55:05 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Ajay")
 
 
-let name = "hitesh"
+let name = "Ajay"
 let age = 18
 let isLoggedIn = false
 let state;
